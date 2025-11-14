cb125a9fc4330f0e1dfff2b54abec8e5fdb1f65f
EmilynSoley3519925 <es3519925@lpssonline.com>
2025-11-14 19:49:57 +0000

    todo 2 complete

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -47,10 +47,10 @@ console.log(createPlatform(850,300,10,300));
        console.log(createCollectable("grace",1000,400));
         console.log(createCollectable("diamond",300,600,));
     // TODO 4 - Create Cannons
-     console.log(createCannon("top",400,1000));
-      console.log(createCannon("top",750,1000));
+     console.log(createCannon("top",400,2000));
+      console.log(createCannon("top",750,2000));
       console.log(createCannon("right",750,2000));
-      console.log(createCannon("top",1100,1000));
+      console.log(createCannon("top",1100,2000));
 
       
 
